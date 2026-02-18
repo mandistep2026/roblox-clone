1f045b11dd95f2554db6a2ac4b84ce7af31293d7
mandistep2026 <mandistep2026@outlook.com>
2026-02-18 23:09:01 +0000

    Fix Window class: revert SDL3 include to SDL and ensure callback functions are properly defined

--- src/renderer/Window.hpp
+++ src/renderer/Window.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <SDL3/SDL.h>
+#include <SDL.h>
 #include <string>
 #include <functional>
 
@@ -23,6 +23,8 @@ public:
     
     void setCloseCallback(std::function<void()> callback) { m_closeCallback = callback; }
     void setResizeCallback(std::function<void(int, int)> callback) { m_resizeCallback = callback; }
+    void setKeyCallback(std::function<void(int, bool)> callback) { m_keyCallback = callback; }
+    void setMouseCallback(std::function<void(int, int, int, bool)> callback) { m_mouseCallback = callback; }
     
     int getWidth() const { return m_width; }
     int getHeight() const { return m_height; }
@@ -38,6 +40,8 @@ private:
     
     std::function<void()> m_closeCallback;
     std::function<void(int, int)> m_resizeCallback;
+    std::function<void(int, bool)> m_keyCallback;
+    std::function<void(int, int, int, bool)> m_mouseCallback;
 };
 
 }
